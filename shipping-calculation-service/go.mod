b7815c0272b3ad12e5485ada297a9a43162f0b9e
tiagosan040 <tiagosan040@gmail.com>
2023-08-26 11:40:22 -0300

    feat (go): configure prometheus and grafana

--- shipping-calculation-service/go.mod
+++ shipping-calculation-service/go.mod
@@ -9,8 +9,21 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.10.1 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.16.0
 	github.com/stretchr/objx v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- shipping-calculation-service/main.go
+++ shipping-calculation-service/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	usecases "github.com/jtiagosantos/vehicle-tracking-app/shipping-calculation-service/internal/shipping/use-cases"
@@ -12,7 +13,39 @@ import (
 	"github.com/jtiagosantos/vehicle-tracking-app/shipping-calculation-service/internal/shipping/entity"
 	"github.com/jtiagosantos/vehicle-tracking-app/shipping-calculation-service/internal/shipping/infra/repository"
 	"github.com/jtiagosantos/vehicle-tracking-app/shipping-calculation-service/pkg/kafka"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	routesCreated = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "routes_created_total",
+			Help: "Total number of created routes",
+		},
+	)
+
+	routesStarted = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "routes_started_total",
+			Help: "Total number of started routes",
+		},
+		[]string{"status"},
+	)
+
+	errorsTotal = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "errors_total",
+			Help: "Total number of errors",
+		},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(routesStarted)
+	prometheus.MustRegister(errorsTotal)
+	prometheus.MustRegister(routesCreated)
+}
 
 func main() {
 	db, err := sql.Open("mysql", "root:root@tcp(host.docker.internal:3306)/routes?parseTime=true")
@@ -22,6 +55,9 @@ func main() {
 	}
 
 	defer db.Close()
+
+	http.Handle("/metrics", promhttp.Handler())
+	go http.ListenAndServe(":8080", nil)
 
 	msgChan := make(chan *ckafka.Message)
 	topics := []string{"routes"}
@@ -39,23 +75,40 @@ func main() {
 
 		switch input.Event {
 		case "RouteCreated":
-			output, err := createRouteUseCase.Execute(input)
+			_, err := createRouteUseCase.Execute(input)
 
 			if err != nil {
 				fmt.Println(err)
+				errorsTotal.Inc()
+			} else {
+				routesCreated.Inc()
 			}
-			fmt.Println(output)
 
-		case "RouteStarted", "RouteFinished":
+		case "RouteStarted":
 			input := usecases.ChangeRouteStatusInput{}
 			json.Unmarshal(msg.Value, &input)
 
-			output, err := changeRouteStatusUseCase.Execute(input)
+			_, err := changeRouteStatusUseCase.Execute(input)
 
 			if err != nil {
 				fmt.Println(err)
+				errorsTotal.Inc()
+			} else {
+				routesStarted.WithLabelValues("started").Inc()
 			}
-			fmt.Println(output)
+
+		case "RouteFinished":
+			input := usecases.ChangeRouteStatusInput{}
+			json.Unmarshal(msg.Value, &input)
+
+			_, err := changeRouteStatusUseCase.Execute(input)
+
+			if err != nil {
+				fmt.Println(err)
+				errorsTotal.Inc()
+			} else {
+				routesStarted.WithLabelValues("finished").Inc()
+			}
 		}
 	}
 }
